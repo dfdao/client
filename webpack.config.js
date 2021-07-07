b169d38c4fccddf914466b7fcf1daa12985ba4ca
Manan Patel <manan19@gmail.com>
2021-07-07 12:34:47 -0700

    change default to rpc-df

--- webpack.config.js
+++ webpack.config.js
@@ -98,7 +98,7 @@ module.exports = {
     // The string values are fallbacks if the env variable is not set
     new EnvironmentPlugin({
       NODE_ENV: 'development',
-      DEFAULT_RPC: 'http://3.211.163.213:8545',
+      DEFAULT_RPC: 'https://rpc-df.xdaichain.com/',
       CONVERSATION_API_HOST: isProd ? 'https://api.zkga.me' : 'http://localhost:3000',
       LEADERBOARD_API: isProd ? 'https://api.zkga.me' : 'http://localhost:3000',
       WEBSERVER_URL: isProd ? 'https://api.zkga.me' : 'http://localhost:3000',
