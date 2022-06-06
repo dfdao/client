02b733697c7471e5e8f3acd49079ae8dfe5d51ef
lermchair <13699109+davdhng@users.noreply.github.com>
2022-06-06 08:59:37 -0400

    do same improvements to the game landing page

--- src/Frontend/Pages/LobbyLandingPage.tsx
+++ src/Frontend/Pages/LobbyLandingPage.tsx
@@ -135,7 +135,7 @@ class LobbyPageTerminal {
       this.terminal.println('Also, clearing browser local storage/cache will render your');
       this.terminal.println('burner wallets inaccessible, unless you export your private keys.');
       this.terminal.println('');
-      this.terminal.println('Press Enter to continue:', TerminalTextStyle.Text);
+      this.terminal.println('Press ENTER to continue:', TerminalTextStyle.Text);
 
       await this.terminal.getInput();
       await this.sendDrip(newAddr);
@@ -163,6 +163,7 @@ class LobbyPageTerminal {
     this.terminal.newline();
     const newSKey = (await this.terminal.getInput()) || '';
     if (newSKey === 'x') {
+      this.terminal.newline();
       this.terminal.println('Cancelled import.', TerminalTextStyle.Text);
       await this.chooseAccount();
       return;
