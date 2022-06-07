79ba909193a7ba9b9c47cad938cc721439a6b9d5
cha0sg0d <0xcha0sg0d@gmail.com>
2022-06-07 15:58:13 -0400

    tweak: link to twitter

--- src/Frontend/Pages/LandingPage.tsx
+++ src/Frontend/Pages/LandingPage.tsx
@@ -100,7 +100,7 @@ export default function LandingPage() {
             <LandingPageRoundArt />
 
             <Spacer height={16} />
-            Happy Birthday Will! - dfdao family
+            6/7 - Happy Birthday 🥳 <Link to={'https://twitter.com/DangerWillRobin'}> Will</Link> ! - dfdao family
             <Spacer height={16} />
             <ButtonWrapper>
               <Btn
