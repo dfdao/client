dd7d8a1f162283794a7e2c1175b5fffdb02ec348
Henry Caron <henryacaron@gmail.com>
2022-07-28 17:59:50 -0400

    fix: blockchain connection bug and routing bug

--- src/Frontend/Pages/EntryPage.tsx
+++ src/Frontend/Pages/EntryPage.tsx
@@ -355,7 +355,7 @@ export function EntryPage() {
               <Redirect path='/play' to={`/play/${defaultAddress}`} push={true} exact={true} />
               <Route path='/play/:contract' component={GameLandingPage} />
               <Redirect path='/portal' to={`/portal/map`} push={true} exact={true} />
-              <Route path='/portal/map' component={PortalPage} />
+              <Route path='/portal' component={PortalPage} />
               <Redirect path='/arena' to={`/arena/${defaultAddress}`} push={true} exact={true} />
               <Route path='/arena/:contract' component={CreateLobby} />
               <Route path='*' component={NotFoundPage} />
