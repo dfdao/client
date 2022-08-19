4bf6775e3c6060b5d25bb11f51a1810792608ef8
Henry Caron <henryacaron@gmail.com>
2022-08-18 20:25:44 -0400

    feat: account view basics implemented

--- src/Frontend/Views/Portal/Account.tsx
+++ src/Frontend/Views/Portal/Account.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import styled from 'styled-components';
 import { logOut } from '../../../Backend/Network/AccountManager';
 import { Gnosis, Icon, IconType, Twitter } from '../../Components/Icons';
@@ -8,7 +8,17 @@ import dfstyles from '../../Styles/dfstyles';
 import { useEthConnection, useTwitters } from '../../Utils/AppHooks';
 import { truncateAddress } from './PortalUtils';
 
+function AccountModal({}: {}) {
+  return (
+    <ModalContainer>
+      <AccountDetails>
+        <span>hello mama</span>
+      </AccountDetails>
+    </ModalContainer>
+  );
+}
 export function Account() {
+  const [open, setOpen] = useState<boolean>(true);
   const connection = useEthConnection();
   const address = connection.getAddress();
   const twitters = useTwitters();
@@ -17,34 +27,39 @@ export function Account() {
   const truncatedAddress = truncateAddress(address);
 
   return (
-    <PaneContainer>
-      <IconContainer>
-        {' '}
-        <button onClick={logOut}>Logout</button>
-      </IconContainer>
-      <a
-        style={{ display: 'flex', alignItems: 'center' }}
-        target='_blank'
-        href={`https://blockscout.com/xdai/optimism/address/${address}`}
-      >
-        <GnoButton>
-          <Gnosis width='24px' height='24px' />
-        </GnoButton>
-      </a>
-      {twitter && (
-        <a
-          style={{ display: 'flex', alignItems: 'center' }}
-          target='_blank'
-          href={`https://twitter.com/${twitter}`}
-        >
-          <Twitter width='24px' height='24px' />
-        </a>
-      )}
-
-      <NamesContainer>{twitter || truncatedAddress}</NamesContainer>
-    </PaneContainer>
+    <>
+      {open && <AccountModal />}
+      <PaneContainer>
+        <NamesContainer onClick={() => setOpen(true)}>{twitter || truncatedAddress}</NamesContainer>
+      </PaneContainer>
+    </>
   );
 }
+
+const ModalContainer = styled.div`
+  position: absolute;
+  width: 100vw;
+  height: 100vh;
+  top: 0;
+  left: 0;
+  background: rgba(0, 0, 0, 0.6);
+  z-index: 1000;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+`;
+
+const AccountDetails = styled.div`
+  min-width: 50%;
+  background: #38383b;
+  border: 1px solid #676767;
+  color: #dddde9;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  padding: 12px;
+  border-radius: 5px;
+`;
 
 const PaneContainer = styled.div`
   padding: 8px;
@@ -65,7 +80,7 @@ const IconContainer = styled.div`
   border-radius: 2px;
 `;
 
-const NamesContainer = styled.div`
+const NamesContainer = styled.button`
   display: flex;
   flex-direction: column;
   justify-content: center;
