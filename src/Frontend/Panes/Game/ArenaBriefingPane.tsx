3fcff4fe1bf2e3d029ba9adc6cccec77ea07d69d
Henry Caron <henryacaron@gmail.com>
2022-09-06 11:54:08 -0400

    feat: show target planet to player

--- src/Frontend/Panes/Game/ArenaBriefingPane.tsx
+++ src/Frontend/Panes/Game/ArenaBriefingPane.tsx
@@ -1,77 +1,115 @@
-import React, {  useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import { Btn } from '../../Components/Btn';
-import { Bronze, Gold, Green, Silver } from '../../Components/Text';
-import { bronzeTime, goldTime, silverTime } from '../../Utils/constants';
+import { Gold, Green } from '../../Components/Text';
 import { useUIManager } from '../../Utils/AppHooks';
-import { formatDuration } from '../../Utils/TimeUtils';
 import { StyledTutorialPane } from './StyledTutorialPane';
+import { setBooleanSetting } from '../../Utils/SettingsHooks';
+import { Setting } from '@darkforest_eth/types';
 
-
+const enum BriefingStep {
+  Welcome,
+  Target,
+  Complete,
+}
 export function ArenaBriefingPane() {
-  const [open, setOpen] = useState(true);
   const uiManager = useUIManager();
+  const [open, setOpen] = useState(uiManager.gameStarted);
+  const [step, setStep] = useState<BriefingStep>(BriefingStep.Welcome);
+
+  const config = {
+    contractAddress: this.uiManager.getContractAddress(),
+    account: this.uiManager.getAccount(),
+  };
   const spectatorMode = uiManager.getGameManager().getIsSpectator();
-  const isSinglePlayer = uiManager.getSpawnPlanets().length == 1 
+  const isSinglePlayer = uiManager.getSpawnPlanets().length == 1;
   const victoryThreshold = uiManager.contractConstants.CLAIM_VICTORY_ENERGY_PERCENT;
   const numForVictory = uiManager.contractConstants.TARGETS_REQUIRED_FOR_VICTORY;
-  const isCompetitive = uiManager.isCompetitive();
-
+  const targetLocation = uiManager.getPlayerTargetPlanets()[0].locationId;
+  const targetCoords = uiManager.getGameManager().getRevealedLocations().get(targetLocation);
+  const homeLocation = uiManager.getHomeHash();
+  useEffect(() => {
+    if (step == BriefingStep.Target) {
+      uiManager.centerLocationId(targetLocation);
+    } else if (step == BriefingStep.Complete) {
+      if (homeLocation) uiManager.centerLocationId(homeLocation);
+    }
+  }, [step]);
 
   if (spectatorMode || !open) {
     return null;
   }
 
+  const welcomeContent = (
+    <div className='tutzoom'>
+      Welcome to Dark Forest Arena!
+      <br />
+      <br />
+      <div>
+        {isSinglePlayer ? (
+          <>
+            Race against the clock to capture the Target Planet (it has a big 🎯 floating above it)
+            and{' '}
+            <Green>
+              claim victory when it contains at least <Gold>{victoryThreshold}%</Gold> energy!
+            </Green>
+          </>
+        ) : (
+          <>
+            Battle your opponent to capture the Target Planet (it has a big 🎯 floating above it)
+            and{' '}
+            <Green>
+              claim victory when it contains at least <Gold>{victoryThreshold}%</Gold> energy!
+            </Green>
+            .
+          </>
+        )}
+        <div>You need {numForVictory} target planets to claim victory.</div>
+      </div>
+      <br />
+      <div style={{ gap: '5px' }}>
+        <Btn className='btn' onClick={() => setOpen(false)}>
+          Close
+        </Btn>
+        <Btn className='btn' onClick={() => setStep(BriefingStep.Target)}>
+          View Target Planet
+        </Btn>
+      </div>
+    </div>
+  );
+
+  const targetContent = (
+    <div className='tutZoom'>
+      <div>
+        This is your objective: the 🎯 Target Planet.{' '}
+        {targetCoords && `It is located at (${targetCoords.coords.x}, ${targetCoords.coords.y}).`}
+      </div>
+      <br />
+      <div>
+        ⏲️ starts {isSinglePlayer ? 'with your first move' : 'when you press ready'}. Good luck!
+      </div>
+      <br />
+      <div style={{ gap: '5px' }}>
+        <Btn
+          className='btn'
+          onClick={() => {
+            setOpen(false);
+            setStep(BriefingStep.Complete);
+            setBooleanSetting(config, Setting.ShowArenaBriefing, true);
+          }}
+        >
+          Return to home planet
+        </Btn>
+      </div>
+    </div>
+  );
+
+  const completeContent = <></>;
+
   return (
     <StyledTutorialPane>
-      <div className='tutzoom'>
-        Welcome to Dark Forest Arena!
-        <br />
-        <br />
-        <div>
-          {isSinglePlayer? (
-            <>
-              Race against the clock to capture the Target Planet (it has a big 🎯 floating above
-              it) and{' '}
-              <Green>
-                claim victory when it contains at least <Gold>{victoryThreshold}%</Gold> energy!
-              </Green>
-            </>
-          ) : (
-            <>
-              Battle your opponent to capture the Target Planet (it has a big 🎯 floating above it)
-              and{' '}
-              <Green>
-                claim victory when it contains at least <Gold>{victoryThreshold}%</Gold> energy!
-              </Green>
-              .
-            </>
-          )}
-          You need {numForVictory} to win.
-        </div>
-        {isCompetitive && isSinglePlayer && (
-          <div>
-            <p>End the race in a certain time to earn Bronze, Silver, and Gold ranks.</p>
-            <p>
-              Gold: <Gold>{formatDuration(goldTime * 1000)}</Gold>
-            </p>
-            <p>
-              Silver: <Silver>{formatDuration(silverTime * 1000)}</Silver>
-            </p>
-            <p>
-              Bronze: <Bronze>{formatDuration(bronzeTime * 1000)}</Bronze>
-            </p>
-          </div>
-        )}
-        <div>
-          ⏲️ starts {isSinglePlayer ? 'with your first move' : 'when you press ready'}!
-          {isCompetitive && isSinglePlayer && 'The player with the fastest time after 48hrs will win XDAI and a 🏆!'}
-        </div>
-        <div style={{ gap: '5px' }}>
-          <Btn className='btn' onClick={() => setOpen(false)}>
-            Next
-          </Btn>
-        </div>
-      </div>
+      {step == BriefingStep.Welcome && welcomeContent}
+      {step == BriefingStep.Target && targetContent}
+      {step == BriefingStep.Complete && completeContent}
     </StyledTutorialPane>
   );
 }
